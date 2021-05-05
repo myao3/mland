1adf50c5e37a74cfa86b14a255baf96514085216
myao3 <69337811+myao3@users.noreply.github.com>
2021-05-04 21:42:35 -0700

    Create module05.js

---
+++ module05.js
@@ -0,0 +1,30 @@
+var table = '';             // Unit of table
+
+
+table = GetValueFromUser("table")
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = GetTableContent(table);
+
+function GetValueFromUser(valueType) {
+  var greetingMessage = 'Hello. How are you? Please enter a number';
+  if (valueType == "table") {}
+
+
+  return prompt(greetingMessage);
+}
+
+function GetTableContent(table) {
+ 
+  var i = 1;                 // Set counter to 1
+  var msg = '';              // Message  
+  
+    while (i < 11) {
+      msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+      i++;
+    }
+  
+  return '<h2>Multiplication Table<h2>'+ '<br/>'+ msg;
+  
+    }
