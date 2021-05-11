060925aadb196eca38086651a0267d0f0ecab0bf
myao3 <69337811+myao3@users.noreply.github.com>
2021-05-10 19:30:04 -0700

    Update artifact03.js

--- artifact03.js
+++ artifact03.js
@@ -91,16 +91,16 @@ document.getElementById("downRight").innerHTML = rLine;
 
 function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
-for (i=pHeight;i=>1;i--){
+for (i=0;i<pHeight-1;i++){
 rLine +="<p>";
   
 //Fill in the left side of every row with spaces or blanks
-for (x=0;x<pHeight-i;x++){
+for (x=0;x<i;x++){
     rLine +="<span class='space'>" +pSymbol +"</span>";
 }
 
 //Create each line on the Rhombus
-for(j=x;j<pHeight*2-1;j++){
+for(j=0;j<pHeight-1;j++){
 
 //Is the position even or odd so we change the color
 if (j%2)
