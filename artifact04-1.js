ac3fe0bbc40cebac46e490d44d113dfdffd864ef
myao3 <69337811+myao3@users.noreply.github.com>
2021-05-17 19:24:04 -0700

    Update artifact04-1.js

--- artifact04-1.js
+++ artifact04-1.js
@@ -1,20 +1,7 @@
-<script  type="text/javascript">
- var frmvalidator = new Validator("myform");
- frmvalidator.addValidation("FirstName","req","Please enter your First Name");
- frmvalidator.addValidation("FirstName","maxlen=20",
-		"Max length for FirstName is 20");
+var frmvalidator  = new Validator("myform");
+  frmvalidator.addValidation("FirstName","maxlen=20",	"Max length for FirstName is 3");
+  frmvalidator.addValidation("FirstName","alpha","Alphabetic chars only");
+  
 
- frmvalidator.addValidation("LastName","req");
- frmvalidator.addValidation("LastName","maxlen=20");
-
- frmvalidator.addValidation("Email","maxlen=50");
- frmvalidator.addValidation("Email","req");
- frmvalidator.addValidation("Email","email");
-
- frmvalidator.addValidation("Phone","maxlen=50");
- frmvalidator.addValidation("Phone","numeric");
-
- frmvalidator.addValidation("Address","maxlen=50");
- frmvalidator.addValidation("Country","dontselect=000");
-
-</script>
+  frmvalidator.addValidation("LastName","maxlen=20","Max length is 3");
+  frmvalidator.addValidation("LastName","alpha","Alphabetic chars only");
