9b493dcc86c031a0f04b7e0d852241bed842b6c7
myao3 <69337811+myao3@users.noreply.github.com>
2021-05-17 19:25:48 -0700

    Update artifact04-1.js

--- artifact04-1.js
+++ artifact04-1.js
@@ -1,7 +1,7 @@
 var frmvalidator  = new Validator("myform");
-  frmvalidator.addValidation("FirstName","maxlen=20",	"Max length for FirstName is 3");
+  frmvalidator.addValidation("FirstName","maxlen=3",	"Max length for FirstName is 3");
   frmvalidator.addValidation("FirstName","alpha","Alphabetic chars only");
   
 
-  frmvalidator.addValidation("LastName","maxlen=20","Max length is 3");
+  frmvalidator.addValidation("LastName","maxlen=3","Max length is 3");
   frmvalidator.addValidation("LastName","alpha","Alphabetic chars only");
