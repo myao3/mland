b7f8bdfa48555b0f633c0a05d68f3ddf0d839b09
myao3 <69337811+myao3@users.noreply.github.com>
2021-05-18 22:03:56 -0700

    Update artifact04-1.js

--- artifact04-1.js
+++ artifact04-1.js
@@ -6,6 +6,10 @@ var FirstName = document.getElementById("FirstName").value;
 var LastName = document.getElementById("LastName").value;
 var Username = document.getElementById("Username").value;
 var Password = document.getElementById("Password").value;
+var Address = document.getElementById("Address").value;
+var City = document.getElementById("City").value;
+var State = document.getElementById("State").value;
+var Country = document.getElementById("Country").value;
 var letters = /^[A-Za-z]+$/;
     
 //3) Do validation
@@ -34,4 +38,30 @@ return (Username);
 }
 return (Password);    
 
+    if ( Address==="null" || Address==="" ){
+    document.getElementById("errorMessages7").innerHTML = "The Address is required";
+    return false;
+}
+return (Address);       
+    
+    if ( City==="null" || City==="" ){
+    document.getElementById("errorMessages8").innerHTML = "The City is required";
+    return false;
+}
+return (City);  
+    
+    if ( State==="null" || State==="000" ){
+    document.getElementById("errorMessages9").innerHTML = "The State is required";
+    return false;
+}
+return (State);       
+    
+    if ( Country==="null" || Country==="000" ){
+    document.getElementById("errorMessages10").innerHTML = "The Country is required";
+    return false;
+}
+return (Country);   
+    
+    
+    
   }
