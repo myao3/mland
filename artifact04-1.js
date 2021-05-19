32d295fbcb15f509e8f158210e38ddb6e2d723ca
myao3 <69337811+myao3@users.noreply.github.com>
2021-05-18 21:51:47 -0700

    Update artifact04-1.js

--- artifact04-1.js
+++ artifact04-1.js
@@ -13,17 +13,20 @@ if ( FirstName==="null" || FirstName==="" || FirstName.length > 3 || !FirstName.
     document.getElementById("errorMessages1").innerHTML = "The firstname is required and cannot be greater than 3 alphabetic character";
     return false;
 }
-
+return true;
+    
 //3) Do validation
 if ( LastName==="null" || LastName==="" || LastName.length > 3 || !LastName.match(letters)){
     document.getElementById("errorMessages2").innerHTML = "The LastName is required and cannot be greater than 3 alphabetic character";
     return false;
 }
+return true;
     
 if ( Username==="null" || Username==="" || Username.length > 3 ){
     document.getElementById("errorMessages5").innerHTML = "The Username is required and cannot be greater than 3 alphabetic character";
     return false;
 }
+return true;
     
 if ( Password==="null" || Password==="" || Password.length > 3 ){
     document.getElementById("errorMessages6").innerHTML = "The Password is required and cannot be greater than 3 alphabetic character";
