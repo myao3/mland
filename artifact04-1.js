473d88c86d7c89276b11a6f677e039881532a03e
myao3 <69337811+myao3@users.noreply.github.com>
2021-05-18 21:41:48 -0700

    Update artifact04-1.js

--- artifact04-1.js
+++ artifact04-1.js
@@ -3,10 +3,11 @@ function validateForm(){
 
 //2) read value from HTML
 var FirstName = document.getElementById("FirstName").value;
-var letters = /^[A-Za-z]+$/;
 var LastName = document.getElementById("LastName").value;
-
-
+var Username = document.getElementById("Username").value;
+var Password = document.getElementById("Password").value;
+var letters = /^[A-Za-z]+$/;
+    
 //3) Do validation
 if ( FirstName==="null" || FirstName==="" || FirstName.length > 3 || !FirstName.match(letters)){
     document.getElementById("errorMessages1").innerHTML = "The firstname is required and cannot be greater than 3 alphabetic character";
@@ -16,6 +17,16 @@ if ( FirstName==="null" || FirstName==="" || FirstName.length > 3 || !FirstName.
 //3) Do validation
 if ( LastName==="null" || LastName==="" || LastName.length > 3 || !LastName.match(letters)){
     document.getElementById("errorMessages2").innerHTML = "The LastName is required and cannot be greater than 3 alphabetic character";
+    return false;
+}
+    
+if ( Username==="null" || Username==="" || Username.length > 3 ){
+    document.getElementById("errorMessages5").innerHTML = "The Username is required and cannot be greater than 3 alphabetic character";
+    return false;
+}
+    
+if ( Password==="null" || Password==="" || Password.length > 3 ){
+    document.getElementById("errorMessages6").innerHTML = "The Password is required and cannot be greater than 3 alphabetic character";
     return false;
 }
 return true;
