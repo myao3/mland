f265a6c493aa77a0189c0df5ef86370874b41f1d
myao3 <69337811+myao3@users.noreply.github.com>
2021-05-18 20:46:17 -0700

    Update artifact04-1.js

--- artifact04-1.js
+++ artifact04-1.js
@@ -4,10 +4,11 @@ function validateForm(){
 //2) read value from HTML
 var FirstName = document.getElementById("FirstName").value;
 //3) Do validation
-if (isAlpha(FirstName) || FirstName==="null" || FirstName==="" || FirstName.length > 3)
+if (isAlpha(FirstName) || FirstName==="null" || FirstName==="" || FirstName.length > 3){
     document.getElementById("errorMessages").innerHTML = "The firstname is required and cannot be greater than 3 alphabetic character";
     return false;
-else
+}
+else{
   return true;
-
+}
   }
