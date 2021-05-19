afb6366b08e75395ce4133424d9dfd410930355e
myao3 <69337811+myao3@users.noreply.github.com>
2021-05-18 19:36:00 -0700

    Update artifact04-1.js

--- artifact04-1.js
+++ artifact04-1.js
@@ -1,13 +1,12 @@
-function FirstName(){
+function validateForm(){
 
 var validFirstName=false;
 //2) read value from HTML
 var FirstName = document.getElementById("FirstName").value;
 //3) Do validation
 if (FirstName==="null" || FirstName==="" || FirstName.length > 3)
-    document.getElementById("errorMessages").innerHTML = "The FirstName is required and cannot be greater than 3 characters";
-    return false
+    errorMessages += "<p>The firstname is required and cannot be greater than 3 characters</p>";
 else
-   return true;
-
+   validFirstname = true;
+return (validFirstname);
   }
