eaa35863ed0cf38873cd6f3dbe3d31ebe52c1189
myao3 <69337811+myao3@users.noreply.github.com>
2021-05-18 19:43:56 -0700

    Update artifact04-1.js

--- artifact04-1.js
+++ artifact04-1.js
@@ -7,7 +7,7 @@ var FirstName = document.getElementById("FirstName").value;
 if (FirstName==="null" || FirstName==="" || FirstName.length > 3)
     errorMessages += "<p>The firstname is required and cannot be greater than 3 characters</p>";
 else
-   validFirstname = true;
+   validFirstName = true;
 document.getElementById("errorMessages").innerHTML = errorMessages;
-return (validFirstname);
+return (validFirstName);
   }
