e28bbba25c490569668e5ff937f4fc378308bf77
myao3 <69337811+myao3@users.noreply.github.com>
2021-05-19 19:09:24 -0700

    Update artifact04.js

--- artifact04.js
+++ artifact04.js
@@ -2,27 +2,19 @@ function validateForm(){
 
 
 //2) read value from HTML
-var FirstName = document.getElementById("FirstName").value;
-var LastName = document.getElementById("LastName").value;
-var Username = document.getElementById("Username").value;
-var Password = document.getElementById("Password").value;
-var Address = document.getElementById("Address").value;
-var City = document.getElementById("City").value;
-var State = document.getElementById("State").value;
-var Country = document.getElementById("Country").value;
-var letters = /^[A-Za-z]+$/;
-var EMail = document.getElementById("EMail").value;
-var atpos = userEmail.indexOf("@");
-var dotpos = userEmail.lastIndexOf(".");
-var phone = document.getElementById("Phone").value;    
 
-if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=EMail.length) {
-    document.getElementById("errorMessages5").innerHTML = "<p>Invalid email</p>";
+
+var Country = document.getElementById("Country").value;
+var Zipcode = document.getElementById("Zipcode").value;
+
+
+if ( Country==="null" || Country==="000" ){
+    document.getElementById("errorMessages10").innerHTML = "The Country is required";
     return false;
 }
      
-if (isNaN(Phone) || Phone.lenght >15 || Phone===null || Phone===""){
-    document.getElementById("errorMessages6").innerHTML= "<p>Invalid phone number </p>";
+if (  Country==="USA" || Zipcode.length > 5){
+    document.getElementById("errorMessages10").innerHTML = "The Zip code is required";
     return false;
 }
 return true; 
