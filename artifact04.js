226816b78771a134b9638ee1ac4f9e92f5db2018
myao3 <69337811+myao3@users.noreply.github.com>
2021-05-19 19:13:00 -0700

    Update artifact04.js

--- artifact04.js
+++ artifact04.js
@@ -9,12 +9,12 @@ var Zipcode = document.getElementById("Zipcode").value;
 
 
 if ( Country==="null" || Country==="000" ){
-    document.getElementById("errorMessages10").innerHTML = "The Country is required";
+    document.getElementById("errorMessages5").innerHTML = "The Country is required";
     return false;
 }
      
 if (  Country==="USA" || Zipcode.length > 5){
-    document.getElementById("errorMessages10").innerHTML = "The Zip code is required";
+    document.getElementById("errorMessages6").innerHTML = "The Zip code is required";
     return false;
 }
 return true; 
