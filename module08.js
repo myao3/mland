d7b8e79164925c929b8b339100d107742dfc85e9
myao3 <69337811+myao3@users.noreply.github.com>
2021-05-23 10:41:46 -0700

    Create module08.js

---
+++ module08.js
@@ -0,0 +1,11 @@
+function addItem(){
+var newItem = document.createElement('item').value;
+var newEl = document.createElement('li');
+var newText = document.createTextNode(newItem);
+
+newEl.appendChild(newText);
+
+var position = document.getElementsByTagName('ul')[0];
+
+position.appendChild(newEl);
+}
